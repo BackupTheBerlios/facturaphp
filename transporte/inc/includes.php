c844383f66905b7013041710b8387f78b6e0b1f3
demetrio <demetrio>
2004-08-02 12:47:33 +0000

    *** empty log message ***

--- transporte/inc/includes.php
+++ transporte/inc/includes.php
@@ -1,6 +1,7 @@
 <?php
 require_once('config.inc.php');
 require_once('users.class.php');
+require_once('groups.class.php');
 global 	$INSTALL_DIR,$SMARTY_DIR, $ADODB_DIR;
 require_once($ADODB_DIR.'adodb.inc.php');
 require_once($SMARTY_DIR.'Smarty.class.php');
