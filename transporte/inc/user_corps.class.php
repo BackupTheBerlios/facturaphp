a1fa968368579a823c7998d1ee031f316898304f
rocio <rocio>
2004-11-09 09:50:33 +0000

    *** empty log message ***

--- transporte/inc/user_corps.class.php
+++ transporte/inc/user_corps.class.php
@@ -1,12 +1,4 @@
 <?php
-//clase que da soporte a los usuarios del programa
-//enlaza con la bbdd 
-global $ADODB_DIR, $INSTALL_DIR;
-require_once ($INSTALL_DIR.'inc/config.inc.php');
-require_once ($INSTALL_DIR.'inc/table.class.php');
-
-require_once ($ADODB_DIR."adodb.inc.php");
-
 
 //clase que da soporte a los usuarios del programa
 //enlaza con la bbdd 
@@ -38,10 +30,26 @@ var $id_corp;
 		$emp = new emps();
 		$num = $emp->get_user_corps($id_user);
 		$tpl->assign('user_corps', $emp->corps_list);	
-		$tpl->assign('plantilla', 'user_corps.tpl');					
+		$tpl->assign('plantilla', 'user_corps_list.tpl');					
 		
 		return $tpl;
 	}
+	
+	function listar($tpl)
+	{
+		$user = new users();
+		$id_user = $user->get_id($_SESSION['user']);
+		$emp = new emps();
+		$num = $emp->get_user_corps($id_user);
+
+		$tabla_listado = new table(true);
+		$cadena=''.$tabla_listado->make_tables('emps',$emp->corps_list,array('Nombre',50),array('id_corp','name'),10,array('view'),true);
+		$variables=$tabla_listado->nombres_variables;		
+		$tpl->assign('variables',$variables);
+		$tpl->assign('cadena',$cadena);		
+		return $tpl;
+	}
+	
 	
 	function bar($method,$corp){		
 		if ($corp != ""){
