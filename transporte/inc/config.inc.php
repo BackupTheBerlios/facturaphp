f43e47c503d16695dccc9442a90e947f5bb2affb
demetrio <demetrio>
2005-01-10 16:53:59 +0000

    *** empty log message ***

--- transporte/inc/config.inc.php
+++ transporte/inc/config.inc.php
@@ -32,6 +32,17 @@
 	$DDBB_PORT="3306";
 	$INSTALL_DIR="D:\\Archivos de programa\\EasyPHP1-7\\www\\transporte\\";*/
 	//**************************************************************
+	//Configuración De Dani (Windows-PORTATIL)
+	
+	$DDBB_NAME="transporte";
+	$DDBB_USER="root";
+	$DDBB_PASS="";
+	$IP_DDBB="127.0.0.1";
+	$TABLE_PREFIX="";
+	$DDBB_TYPE="mysql";
+	$DDBB_PORT="3306";
+	$INSTALL_DIR="C:\\wamp\\www\\transporte\\";
+	//**************************************************************
 	//Configuración De Dani (Linux)
 /*	
 	$DDBB_NAME="transporte";
@@ -42,9 +53,10 @@
 	$DDBB_TYPE="mysql";
 	$DDBB_PORT="3306";
 	$INSTALL_DIR="/var/www/transporte/";	*/
+	
 	//**************************************************************
 	//Configuración De Rocío (Linux)
-	
+	/*	
 	$DDBB_NAME="transporte";
 	$DDBB_USER="root";
 	$DDBB_PASS="sta3war2";
@@ -53,7 +65,7 @@
 	$DDBB_TYPE="mysql";
 	$DDBB_PORT="3306";
 	$INSTALL_DIR="/var/www/transporte/";
-
+*/
 	// *************************************************
 	//Configuracion de David
 /*
@@ -80,14 +92,19 @@
 	$INSTALL_DIR="C:\\Archivos de programa\\EasyPHP1-7\\www\\transporte\\";
 	*/
 	
-	$WIN32=false;
-	$SLASH="/";
-	$ADODB_DIR=$INSTALL_DIR."inc/adodb/";
-	$SMARTY_DIR=$INSTALL_DIR."inc/smarty/";
-	$SMARTY_TEMPLATES_DIR=$INSTALL_DIR."templates/";
-	$SMARTY_TEMPLATES_C_DIR=$INSTALL_DIR."templates_c/";
-	$SMARTY_CONFIGS_DIR=$INSTALL_DIR."configs/";
-	$SMARTY_CACHE_DIR=$INSTALL_DIR."cache/";
+	//$WIN32=false;
+	$WIN32=true;
+	$BARRA_WINDOWS="\\";
+	$BARRA_LINUX="/";
+	//$SLASH="/";
+	$SLASH=$BARRA_WINDOWS;
+	//$SLASH=$BARRA_LINUX;
+	$ADODB_DIR=$INSTALL_DIR."inc".$SLASH."adodb".$SLASH;
+	$SMARTY_DIR=$INSTALL_DIR."inc".$SLASH."smarty".$SLASH;
+	$SMARTY_TEMPLATES_DIR=$INSTALL_DIR."templates".$SLASH;
+	$SMARTY_TEMPLATES_C_DIR=$INSTALL_DIR."templates_c".$SLASH;
+	$SMARTY_CONFIGS_DIR=$INSTALL_DIR."configs".$SLASH;
+	$SMARTY_CACHE_DIR=$INSTALL_DIR."cache".$SLASH;
 	$WEB_SERVER="";
 	$WEB_DIR="";
 	$BASE_URL="";
