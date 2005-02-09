273b3f105ecb8d8181adf7279166dd2c74b356c6
rocio <rocio>
2005-02-09 12:52:14 +0000

    *** empty log message ***

--- transporte/index.php
+++ transporte/index.php
@@ -1,5 +1,4 @@
 <?php
-
 //Comprueba si existe la bbdd
 //si no existe la bbdd va a la instalación
 //aqui debe de irse a la instalación de la aplicación
@@ -36,10 +35,14 @@ if(!isset($_SESSION['user']))
 	$post_user= new users(); 
 	if(isset($_POST['passwd'])&& isset($_POST['user'])&&$post_user->validate_user($_POST['user'],$_POST['passwd'])==1)
 	{
+		//Se busca registro de la sesión (ya se conoce al usuario)
 		$session=new sessions();
 		$session->register();
+		
 		//registra la variable de sesion user con el nombre de usuario
 		$_SESSION['user']=$_POST['user'];
+		
+		//Comprueba si es admin o super
 		$user = new users();
 		$id_user = $user->get_id($_SESSION['user']);
 		$num_groups = $user->get_groups($id_user);
@@ -62,7 +65,18 @@ if(!isset($_SESSION['user']))
 			}
 		}
 
+		//Inicia empresas a 0 (no se conoce empresa con la que se quiere trabajar)
 		$_SESSION['ident_corp'] = 0;
+		
+		//Se crea coockie
+		if(!isset($_COOKIE[session_name()]))
+       	{
+           // Gives the user 30 seconds to type the password.
+           // Should be enough :-)
+           setcookie(session_name(), 1,time()+30);
+       	}
+		
+		//Se busca registro de la sesión (ya se conoce al usuario) y se introduce al usuario
 		
 		//como el usuario esta validado asigna su nombre a la plantilla
 		$tpl->assign('user_name',$_SESSION['user']);
@@ -98,6 +112,8 @@ else
 	//usuario registrado en la sesion
 	if(isset($_POST['submit'])&& $_POST['submit']=='Desconectar')
 	{
+		unset($_COOKIE[session_name()]);
+		session_unset();
 		session_destroy();
 		session_start();
 		$tpl->assign('user_name','');
